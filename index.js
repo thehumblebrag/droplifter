82b800dd49fa6a4ebe0cde538db78cb8dfadcb24
Jimmy Hillis <jimmy@hillis.me>
2014-08-17 11:19:11 +0800

    Add singleton droplifter to manage all internal module dependencies by wrapping express, routes and models

--- index.js
+++ index.js
@@ -5,17 +5,50 @@
  * object of required configraution including:
  *
  * - port
+ * - database
  */
 
+var _ = require('underscore');
+var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
+var config = require(path.join(__dirname, argv.config || 'config'));
 var express = require('express');
-var config = require('./config');
-var app = express();
 
-app.set('config', config);
+var Droplifter = function (config) {
+    this._config = _.extend({
+        port: 3000,
+        database: 'mongodb://localhost/droplifter'
+    }, config);
+    console.log('.');
+    this.express = express();
+};
 
-var routes = require('./routes')(app);
+Droplifter.prototype.get = function (prop) {
+    if (prop in this._config) {
+        return this._config[prop];
+    }
+    return null;
+};
 
-app.listen(process.env.PORT || config.port, function () {
-    console.log('Wait for the drop.');
-});
+Droplifter.prototype.set = function (prop, value) {
+    if (prop in this._config) {
+        this._config[prop] = value;
+    }
+    else {
+        console.error('That configuration value doesn\'t exist');
+    }
+};
+
+Droplifter.prototype.drop = function () {
+    this.routes = require('./routes');
+    this.database = require('./lib/database');
+    this.express.listen(process.env.PORT || this._config.port, function () {
+        console.log('Wait for the drop.');
+    });
+};
+
+var droplifter = module.exports = exports = new Droplifter(config);
+
+droplifter.drop();
+
+module.exports = droplifter;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
+var droplifter = require('../');
 var drop = require('./api/drop');
 var user = require('./api/user');
 
-module.exports = function (app) {
-    // drops
-    app.get('/drop', drop.get);
-    app.get('/drop/:id', drop.find);
-    app.get('/drop/location/:location', drop.geoFind);
-    // // users
-    app.get('/user', user.get);
-    app.get('/user/:id', user.find);
-    app.get('/user/location/:location', user.geoFind);
-};
+// Auth routes
+
+// Drop routes
+droplifter.express.get('/drop', drop.get);
+droplifter.express.get('/drop/:id', drop.find);
+droplifter.express.get('/drop/location/:location', drop.geoFind);
+
+// User routes
+droplifter.express.get('/user', user.get);
+droplifter.express.get('/user/:id', user.find);
+droplifter.express.get('/user/location/:location', user.geoFind);
