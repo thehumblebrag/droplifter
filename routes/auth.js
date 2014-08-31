78355eacef2450d12ec70ae8cc552bc8b6746287
Simon Manning <simon@ecksd.com>
2014-08-31 20:14:03 +0800

    Only set up passport if app keys are present in config.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 // Authentication using passportjs.
 // Supporting Twitter, Facebook and G+ strategies.
 
+var droplifter = require('../');
 var passport = require('passport');
 var config = require('../config');
 var User = require('../models/user');
@@ -15,55 +16,59 @@ passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
-// Facebook
-passport.use(new FacebookTokenStrategy({
-    clientID: config.facebook_id,
-    clientSecret: config.facebook_secret
-},
-function(accessToken, refreshToken, profile, done) {
-    // Find or create user.
-    User.findOne({
-        external_id: profile.id,
-        external_type: 'facebook'
-    }, function (err, user) {
-        if (err) {
-            console.log('err: ' + err);
-            return done(err);
-        }
-        if (!user) {
-            console.log('Creating new user with FB ID: ' + profile.id);
-            user = new User({
-                external_id: profile.id,
-                external_type: 'facebook'
-            });
-            return user.save(done);
-        }
-        done(null, user);
-    });
-}));
+if (droplifter.get('facebook_id')) {
+    // Facebook
+    passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook_id,
+        clientSecret: config.facebook_secret
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // Find or create user.
+        User.findOne({
+            external_id: profile.id,
+            external_type: 'facebook'
+        }, function (err, user) {
+            if (err) {
+                console.log('err: ' + err);
+                return done(err);
+            }
+            if (!user) {
+                console.log('Creating new user with FB ID: ' + profile.id);
+                user = new User({
+                    external_id: profile.id,
+                    external_type: 'facebook'
+                });
+                return user.save(done);
+            }
+            done(null, user);
+        });
+    }));
+}
 
-// Twitter
-passport.use(new TwitterTokenStrategy({
-    consumerKey: config.twitter_key,
-    consumerSecret: config.twitter_secret,
-    skipExtendedUserProfile: true
-},
-function(token, tokenSecret, profile, done) {
-    // Find or create user.
-    User.findOne({
-        external_id: profile.id,
-        external_type: 'twitter'
-    }, function (err, user) {
-        if (err) {
-            return done(err);
-        }
-        if (!user) {
-            user = new User({
-                external_id: profile.id,
-                external_type: 'twitter'
-            });
-            return user.save(done);
-        }
-        done(null, user);
-    });
-}));
+if (droplifter.get('twitter_key')) {
+    // Twitter
+    passport.use(new TwitterTokenStrategy({
+        consumerKey: config.twitter_key,
+        consumerSecret: config.twitter_secret,
+        skipExtendedUserProfile: true
+    },
+    function(token, tokenSecret, profile, done) {
+        // Find or create user.
+        User.findOne({
+            external_id: profile.id,
+            external_type: 'twitter'
+        }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                user = new User({
+                    external_id: profile.id,
+                    external_type: 'twitter'
+                });
+                return user.save(done);
+            }
+            done(null, user);
+        });
+    }));
+}
